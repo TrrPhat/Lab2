f661157abd596c6a8935a3b65e89cb47fb5e1c9e
Trường Phát <lainguyentruongphat006@gmail.com>
2025-09-16 22:34:54 +0700

    Add files via upload

---
+++ Bai3.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Xuất số thành chữ</title>
+</head>
+
+<body>
+    <?php 
+    if(isset($_POST["so"])){
+        if(is_numeric($_POST["so"])){
+            switch($_POST["so"]){
+                case 0: {
+                    $chu = "Khong";
+                    break;
+                }
+                case 1: {
+                    $chu = "Mot";
+                    break;
+                }
+                case 2: {
+                    $chu = "Hai";
+                    break;
+                }
+                case 3: {
+                    $chu = "Ba";
+                    break;
+                }
+                case 4: {
+                    $chu = "Bon";
+                    break;
+                }
+                case 5: {
+                    $chu = "Nam";
+                    break;
+                }
+                case 6: {
+                    $chu = "Sau";
+                    break;
+                }
+                case 7: {
+                    $chu = "Bay";
+                    break;
+                }
+                case 8: {
+                    $chu = "Tam";
+                    break;
+                }
+                case 9: {
+                    $chu = "Chin";
+                    break;
+                }
+                default: {
+                    $chu = "Khong hop le !";
+                    break;
+                }
+            }
+        }
+    }
+    ?>
+<form action="Bai3.php" method="POST" >
+<table width="519" border="1">
+<tr>
+<td colspan="3">Đọc số</td>
+</tr>
+<tr>
+<td>Nhập số (0-9)</td>
+<td width="69" rowspan="2"><input type="submit" name="button" id="button" value="Submit" /></td>
+<td> Bằng chữ</td>
+</tr>
+<tr>
+<td width="177"><p>
+<label for="textfield"></label>
+<input type="text" name="so" id="textfield" />
+</p></td>
+<td width="232"><label for="textfield2"></label>
+<input type="text" name="chu" id="textfield2" value = "<?php echo $chu ?>" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
